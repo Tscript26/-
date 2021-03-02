c7d1bf3c93224aec90828a5538e06b70e8d6257b
zhuyilun <yilun.zhu@aminer.cn>
2021-03-02 17:46:35 +0800

    [add] add demo

--- chess/game.py
+++ chess/game.py
@@ -1,10 +1,12 @@
 BOUND = 10
+import numpy as np
 
 
 class Board:
     def __init__(self):
-        self.board = [[] * 10] * 10
+        self.board = np.zeros([10, 10])
         self.color = {'black': -1, 'white': 1, 'blank': 0}
+        self.last_move = None
         self.blank = 0
 
     def move(self, pos, color):
@@ -18,56 +20,106 @@ class Board:
             return False
         else:
             self.board[x][y] = self.color[color]
-            self.win()
+            print(self.win())
+            self.last_move = color
             return True
 
     def win_con1(self, pos):
-        x,y = pos
-        if self.board[x][y]:
+        x, y = pos
+        if self.board[x][y] != 0:
             color = self.board[x][y]
-            if y < BOUND - 5:
-                state = False
-                for piece in self.board[x][y+1:y+5]:
-                    if piece != color:
-                        return False
+            state = 1
+            for i in range(1, 5):
+                y_ = y + i
+                if y_ > 9 or y_ < 0:
+                    break
+                piece = self.board[x][y_]
+                if piece == color:
+                    state += 1
+                else:
+                    break
+            for i in range(1, 5):
+                y_ = y - i
+                if y_ > 9 or y_ < 0:
+                    break
+                piece = self.board[x][y_]
+                if piece == color:
+                    state += 1
+                else:
+                    break
+            if state >= 5:
                 return True
         return False
 
     def win_con2(self, pos):
-        x,y = pos
+        x, y = pos
         if self.board[x][y]:
             color = self.board[x][y]
-            if x < BOUND - 5:
-                state = False
-                for piece in self.board[x+1:x+5][y]:
-                    if piece != color:
-                        return False
+            state = 1
+            for i in range(1, 5):
+                x_ = x + i
+                if x_ > 9 or x_ < 0:
+                    break
+                piece = self.board[x_][y]
+                if piece == color:
+                    state += 1
+                else:
+                    break
+            if state >= 5:
                 return True
         return False
 
     def win_con3(self, pos):
-        x,y = pos
+        x, y = pos
         if self.board[x][y]:
             color = self.board[x][y]
-            if x < BOUND - 5 and y < BOUND - 5:
-                state = False
-                for i in range(1,5):
-                    piece = self.board[x + i][y + i]
-                    if piece != color:
-                        return False
+            state = 1
+            for i in range(1, 5):
+                x_, y_ = x - i, y - i
+                if x_ > 9 or x_ < 0 or y_ > 9 or y_ < 0:
+                    break
+                piece = self.board[x_][y_]
+                if piece == color:
+                    state += 1
+                else:
+                    break
+            for i in range(1, 5):
+                x_, y_ = x + i, y + i
+                if x_ > 9 or x_ < 0 or y_ > 9 or y_ < 0:
+                    break
+                piece = self.board[x_][y_]
+                if piece == color:
+                    state += 1
+                else:
+                    break
+            if state >= 5:
                 return True
         return False
 
     def win_con4(self, pos):
-        x,y = pos
+        x, y = pos
         if self.board[x][y]:
             color = self.board[x][y]
-            if 3 < x and y < BOUND - 5:
-                state = False
-                for i in range(1,5):
-                    piece = self.board[x - i][y + i]
-                    if piece != color:
-                        return False
+            state = 1
+            for i in range(1, 5):
+                x_, y_ = x - i, y + i
+                if x_ > 9 or x_ < 0 or y_ > 9 or y_ < 0:
+                    break
+                piece = self.board[x_][y_]
+                if piece == color:
+                    state += 1
+                else:
+                    break
+            for i in range(1, 5):
+                x_, y_ = x - i, y + i
+                if x_ > 9 or x_ < 0 or y_ > 9 or y_ < 0:
+                    break
+                piece = self.board[x_][y_]
+                if piece == color:
+                    state += 1
+                else:
+                    break
+            if state >= 5:
                 return True
         return False
 
@@ -75,12 +127,14 @@ class Board:
         for x in range(10):
             for y in range(10):
                 if self.board[x][y] != 0:
-                    pos = [x,y]
-                    color = 'black' if self.board[x][y] ==-1 else 'white'
+                    pos = [x, y]
+                    color = 'black' if self.board[x][y] == -1 else 'white'
                     if self.win_con1([x, y]) or self.win_con2([x, y]) or self.win_con3([x, y]) or self.win_con4([x, y]):
                         return f"{color} win."
         return None
 
 
-b = Board()
-print(b.board)
+if __name__ == '__main__':
+    b = Board()
+    b.move([1, 1], 'white')
+    print(b.board)
